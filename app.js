c5c8b12910857abd119f8dd66982312157b6b5a5
AlohaJarren <calizo.jarren@gmail.com>
2017-11-22 14:05:29 -1000

    added multiple || to conditions, finalized (2)

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var output;
 function calculator (x,y,z) {
-  if (x==="+") {
+  if (x==="+" || x==="sum" || x==="add" || x==="addition" || x==="plus") {
     output = y+z;
-  } else if (x==="-") {
+  } else if (x==="-" || x==="difference" || x==="subtract" || x==="subtraction" || x==="minus") {
     output = y-z;
-  } else if (x==="*") {
+  } else if (x==="*" || x==="product" || x==="multiply" || x==="multiplication") {
     output = y*z;
-  } else if (x==="/") {
+  } else if (x==="/" || x==="quotient" || x==="divide" || x==="division") {
     output = y/z;
   } else {
     output = "Mayn, you gotta set da right operata";
   }
 }
 
-calculator ("/", 69, 42);
+calculator ("divide", 69, 42);
 
 //calculator ("*", 69, 42);
 
